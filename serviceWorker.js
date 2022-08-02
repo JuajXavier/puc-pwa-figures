5d48f669aeef6c2680d32a1cc7297f8e45882cd0
JuajXavier <jvitorxavier@gmail.com>
2022-08-02 04:11:12 -0300

    Add serviceWorker activation/instalation

--- serviceWorker.js
+++ serviceWorker.js
@@ -17,3 +17,37 @@ const FILES_TO_CACHE = [
     'offline.html'
 
 ];
+
+//Instalação do service worker
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker em instalação');
+
+    event.waitUntil(
+
+        // Verifica se existe o cache_name e aí adiciona os arquivos do files to cache nele.
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Service Worker está adicionando o cache estático.');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+// Ativando o SW
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker em ativação');
+
+    event.waitUntil(
+        caches.keys().then((keylist) => {
+            return Promise.all(keylist.map((key) => {
+                if(key !== CACHE_NAME) {
+                    caches.delete(key); 
+                }
+            }))
+        })
+    )
+    // Propagar pra todos os usuários que tem esse SW instalado.
+    self.clients.claim();
+});
