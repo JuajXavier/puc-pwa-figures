8e103dfb0f879123e4097e99d81f83e816444e81
JuajXavier <jvitorxavier@gmail.com>
2022-08-01 22:06:29 -0300

    Dynamic data with ajax

--- js/app.js
+++ js/app.js
@@ -24,11 +24,49 @@ ajax.onreadystatechange = function() {
 
             for(let i = 0; i<jsonData.length; i++) {
 
-               htmlContent = `<div class="row"><div class="col-12"><h2><span></span>${jsonData[i].categoria}</h2></div></div>`
-        
+               htmlContent += `<div class="row"><div class="col-12"><h2><span></span>${jsonData[i].categoria}</h2></div></div>`
+                
+               if (jsonData[i].figures.length == 0) {
+                    htmlContent += '<div class="row"><div class="col-12"><div class="alert alert-warning" role="alert">Desculpe! Não há figures nesta categoria.</div></div></div>'
+               } else {
+
+                   htmlContent += '<div class="row">';
+
+                    for(let j = 0; j<jsonData[i].figures.length; j++) {
+
+                    const figuresInfo = jsonData[i].figures[j];
+
+                    const figureParams = {
+                        nome: figuresInfo.nome,
+                        imagem: figuresInfo.imagem,
+                        valor: figuresInfo.valor,
+                        whatsapp: figuresInfo.whatsapp,
+                    }
+
+                    htmlContent += figureCard(figureParams);
+                }
+                    htmlContent += '</div>';
+               }
             }
 
             content.innerHTML = htmlContent;
         }
     }
+}
+
+//Template do card
+
+var figureCard = function({nome, imagem, valor, whatsapp}) {
+    return  '<div class="col-lg-6">'+
+                '<div class="card">'+
+                    `<img src=${imagem} class="card-img-top" alt="Chopper">`+
+                    '<div class="card-body">'+
+                        `<h5 class="card-title">${nome}</h5>`+
+                        `<p class="card-text"><strong>Valor: </strong>${valor}</p>`+
+                        '<div class="d-grid gap-2">'+
+                            `<a href="https://api.whatsapp.com/send?phone=55${whatsapp}&text=Olá, gostaria de infos sobre a figure: ${nome}" target="_blank" class="btn btn-warning">Contato do proprietário</a>`+
+                        '</div>'+
+                    '</div>'+
+                '</div>'+
+            '</div>'
 }
