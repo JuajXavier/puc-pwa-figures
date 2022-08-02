e8d6918e3639f8b637fbeb8ea6327f4a12a3d4cd
JuajXavier <jvitorxavier@gmail.com>
2022-08-02 03:52:17 -0300

    Add serviceWorker and offline files

---
+++ serviceWorker.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const CACHE_NAME = 'figure-app-estatico';
+
+const FILES_TO_CACHE = [
+
+    'css/bootstrap.min.css',
+    'css/styles.css',
+    'images/favicon.ico',
+    'icons/144.png',
+    'images/logo.png',
+    'images/bg001.jpg',
+    'images/bg002.jpg',
+    'images/cat-icon.png',
+    'js/app.js',
+    'js/bootstrap.bundle.min.js',
+    'offline.html'
+
+];
